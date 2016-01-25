b6a43f8bf34e57ec1d1044bf7d81d9ff44df7d6c
Marcus Battle <marcusbattle@Marcbook.local>
2016-01-25 02:01:37 -0500

    first commit

---
+++ home.php
@@ -0,0 +1,119 @@
+<?php get_header(); ?>
+	<div id="stage">
+		<header>
+			<div id="hero">
+				<div class="L cushion-left-5 cushion-right-5">
+					<div id="nav-bar" class="cushion-top-5 cushion-bottom-5">
+						<h1 class="logo align-center"><a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/she-tank-stacked-web.png" /></a></h1>
+						<nav class="sticker stick-bottom-10 align-center">
+							<ul>
+								<li><a href="#about">about</a></li>
+								<li><a href="#pitch">pitch idea</a></li>
+								<li><a href="#schedule">schedule</a></li>
+								<li><a href="#speakers">speakers</a></li>
+								<li><a href="#register">register</a></li>
+								<li><a href=#contact>contact</a></li>
+							</ul>
+						</nav>
+						<div class="overlay"></div>
+					</div>
+				</div>
+				<div class="R W50 cushion-left-5 cushion-right-5 cushion-top-5 cushion-bottom-5 align-right">
+					<div id="conference-details" class="sticker stick-bottom-10">
+						<p class="special-message highlight pink">
+							Finally, a conference<br />
+							helping the everyday woman<br />
+							balance business and home
+						</p>
+						<h2>March 12th, 2016</h2>
+						<p>
+							DoubleTree Hotel<br />
+							3030 W Gate City Blvd<br />
+							Greensboro, NC 27403
+						</p>
+						<a class="button pink" href="#about">Learn More</a>
+					</div>
+				</div>
+			</div>
+		</header>
+		<section id="about">
+			<div class="inner cushion-left-25 cushion-right-25 cushion-top-5 cushion-bottom-5">
+				<h2 class="align-center">What is SheTank?</h2>
+				<p>It’s no secret that women face some of the greatest barriers to entry in the workplace and for the woman who has children, student loans, juggling home and work, resources are limited. SheTank is a one-day think tank to help aspiring women entrepreneurs learn how to successfully start or grow their businesses, while balancing other responsibilities. Filled with exciting speakers, fun activities and even an business picth contest, SheTank is the support you’ve been waiting for.</p>
+			</div>
+		</section>
+		<section id="pitch">
+			<div class="inner cushion-top-5 cushion-bottom-5 cushion-left-10 cushion-right-10">
+				<h2 class="highlight pink">One Pitch. $1500 Investment.</h2>
+				<p class="special-message highlight">
+					You’ve worked hard enough<br />
+					by yourself. Give yourself a financial<br />
+					break to get ahead.
+				</p>
+			</div>
+		</section>
+		<section id="pitch-details">
+			<div class="inner cushion-left-25 cushion-right-25 cushion-top-5 cushion-bottom-5">
+				<h2 class="align-center pink">Pitch Your Idea</h2>
+				<p>Do you have a great business idea and could use an investment? If you want your business idea to be heard in front of the “ShePanel” for a chance to win $1500, just e-mail your 2-minute video by February 19, 2016 to info@shetank.com discussing your business or idea. Keep the following things in mind:</p>
+				<ol>
+					<li>Video must be 2-minutes or less</li>
+					<li>No late submissions accepted</li>
+					<li>Registration is required to pitch</li>
+					<li>Contestants will be able to pitch their idea in front of the She Tank Panel on March 12, 2016!</li>
+				</ol>
+				<p class="align-center"><a class="button pink" href="mailto:info@shetank.com?subject=Pitch Submission">Pitch Your Idea</a></p>
+			</div>
+		</section>
+		<section id="schedule">
+			<div class="inner">
+				<div class="L W50 cushion-top-5 cushion-bottom-5 cushion-left-10 cushion-right-10">
+					<h2 class="highlight teal">Nine AM to Noon</h2>
+					<p class="special-message highlight grey">Seminar. Four Stories. Four ways to succeed.</p>
+				</div>
+				<div class="L W50 cushion-top-5 cushion-bottom-5 cushion-left-10 cushion-right-10">
+					<h2 class="highlight teal">One PM to Three</h2>
+					<p class="special-message highlight grey">Pitch Contest. Pitch to win.</p>
+				</div>
+			</div>
+		</section>
+		<section id="schedule-details">
+			<div class="inner cushion-left-25 cushion-right-25 cushion-top-5 cushion-bottom-5">
+				<h2 class="teal align-center">The Schedule</h2>
+				<p>On Saturday March 12th, 2016, this one-day intensive workshop will be held in Greensboro, NC at the DoubleTree Hotel. Registration is only $75 and includes your access to learn from four successful women as well as refine and pitch your business idea for a chance to win $1500.</p>
+			</div>
+			<div id="speakers" class="inner cushion-left-10 cushion-right-10">
+				<h2 class="grey align-center">Four Dynamic Speakers</h2>
+				<div class="L W25 align-center">
+					<p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/reketta-wright.png" /></p>
+					<h4>Reketta Wright</h4>
+					<p class="cushion-left-5 cushion-right-5">Grew a mental health practice to over 6 figures</p>
+				</div>
+				<div class="L W25 align-center">
+					<p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/denise-wiggins.png" /></p>
+					<h4>Denise Wiggins</h4>
+					<p class="cushion-left-5 cushion-right-5">Author who turned cosmetic line owner</p>
+				</div>
+				<div class="L W25 align-center">
+					<p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/antonina-griffin.png" /></p>
+					<h4>Antonina Griffin</h4>
+					<p class="cushion-left-5 cushion-right-5">Motivational spaker featured on CBN</p>
+				</div>
+				<div class="L W25 align-center">
+					<p><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/tracey-wolfe.png" /></p>
+					<h4>Tracey Wolfe</h4>
+					<p class="cushion-left-5 cushion-right-5">Manages the image of successful people</p>
+				</div>
+			</div>
+			<div id="register" class="inner cushion-top-5 cushion-bottom-5">
+				<p class="align-center"><a class="button huge teal" href="#">Register Today</a></p>
+			</div>
+		</section>
+		<footer id="contact" class="align-center">
+			<div class="inner cushion-top-5 cushion-bottom-5 cushion-left-10 cushion-right-10">
+				<h2>Questions? Send us a message at info@shetank.com</h2>
+				<p>&copy; 2016. Reketta Wright. All Rights Reserved • Branded by Battle Branding </p>
+			</div>
+		</footer>
+	</div>
+<?php get_footer(); ?>
